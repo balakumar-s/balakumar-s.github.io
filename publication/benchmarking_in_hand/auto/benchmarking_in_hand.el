a4fcc67b30164e72604286408ab34f6f7158a7fa
Balakumar Sundaralingam <s.balakumar@outlook.com>
2020-01-22 19:05:17 -0700

    added icra 2020 paper

---
+++ publication/benchmarking_in_hand/auto/benchmarking_in_hand.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "benchmarking_in_hand"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "cruciani-ral2020-benchmarking-in-hand"))
+ :bibtex)
+
---
+++ publication/recon_grasp/auto/recon_grasp.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "recon_grasp"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "vandermerwe-icra2020-reconstruction-grasping"))
+ :bibtex)
+
